46ae3e361b9816f36839601105bc2a4c408c63dc
ClaudioEto <115515095+ClaudioEto@users.noreply.github.com>
2024-07-05 21:53:54 -0300

    Add files via upload
    JOAO QUE FEZ

---
+++ masqueicos-main/masqueicos-main/README.md
@@ -0,0 +1,1 @@
+# DATA ENTREGA 04/07
---
+++ masqueicos-main/masqueicos-main/Sistema _estoque/Login.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Login{
+    private String Email;
+    private String Senha;
+
+public Login(String Email, String Senha) {
+    this.Email = Email;
+    this.Senha = Senha;
+}
+
+public void ListaLog() {
+    public static void
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Email"+ this.Email);
+        System.out.println("Senha"+ this.Senha);
+        System.out.println("- Login de Usuário -");
+        System.out.println("Digite seu Email: ");
+        String Email = sc.next();
+        System.out.println("Digite sua Senha: ");
+        String Senha = sc.next();
+
+        System.out.println("Login realizado com sucesso!");
+
+    
+
+}
+}
+
+
